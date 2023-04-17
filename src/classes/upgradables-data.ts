37a4c3420a72114b561b91d580190a4dee195974
Nicolas Broca <n.broca@gmail.com>
2023-04-18 00:44:06 +1000

    fix lores of some upgradables

--- src/classes/upgradables-data.ts
+++ src/classes/upgradables-data.ts
@@ -9,14 +9,14 @@ export const upgradablesData: UpgradableStats[] = [
   },
   {
     name: 'Employee',
-    lore: 'Hire employees and equip them with their own button. The job description is simple: "press the button".Each employee presses the button once per second.',
+    lore: 'Hire employees and equip them with their own button. The job description is simple: "press the button". Each employee presses the button once per second.',
     basePrice: 10,
     baseProduction: .1,
     baseUpgradeCost: 100,
     startingQuantity: 0
   }, {
-    name: 'Robot',
-    lore: 'Don\'t imagine something fancy here. These robots are actually some kind of stick positioned above a button and a mechanic that regularly presses it. Still, of course, they are quite better than humans at doing it.',
+    name: 'Robot Prototype',
+    lore: 'Don\'t imagine anything fancy here. These "robots" are just made a of gear, a rubber band and a stick that press a button. Still, of course, they are quite better than humans at doing it.',
     basePrice: 500,
     baseProduction: .5,
     baseUpgradeCost: 5000,
